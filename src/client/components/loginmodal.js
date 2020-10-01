5ea55a2a805abd44ab14330745283362964f1fd8
artiart <art.kouznetsov@gmail.com>
2020-10-01 10:39:36 +0200

    login Modal refactored

--- src/client/components/loginmodal.js
+++ src/client/components/loginmodal.js
@@ -1,104 +1,69 @@
 import React from "react";
 
-class LoginModal extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            showSignup: false,
-            email: "",
-            password: "",
-            color: "#FF0000",
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.toggleSignup = this.toggleSignup.bind(this);
-    }
-    handleChange(event) {
-        const {name, value} = event.target;
-        this.setState({
-            [name]: value,
-        });
-    }
-    toggleSignup() {
-        this.setState({
-            showSignup: !this.state.showSignup,
-        });
-    }
-    handleClick() {
-        console.log("the Class based component method handleClick works");
-    }
-    render() {
-        return (
-            <div id="login-modal">
-                <h2>{this.state.showSignup ? "Sign Up" : "Log In"}</h2>
-                <input
-                    type="text"
-                    name="email"
-                    id="login-modal-id"
-                    placeholder="e-mail"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                    required
-                />
-                <input
-                    type="password"
-                    name="password"
-                    id="login-modal-email"
-                    placeholder="password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    minLength="4"
-                    required
-                />
-                {this.state.showSignup && (
-                    <div id="color-selector">
-                        <p>pick a color :&nbsp;</p>
-                        <input
-                            type="color"
-                            id="color-input"
-                            name="color"
-                            value={this.state.color}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                )}
-                <div id="loginmodal-buttons">
+function LoginModal(props) {
+    return (
+        <div id="login-modal">
+            <h2>{props.state.showSignup ? "Sign Up" : "Log In"}</h2>
+            <input
+                type="text"
+                name="email"
+                id="login-modal-id"
+                placeholder="e-mail"
+                value={props.state.email}
+                onChange={props.handleChange}
+                required
+            />
+            <input
+                type="password"
+                name="password"
+                id="login-modal-email"
+                placeholder="password"
+                value={props.state.password}
+                onChange={props.handleChange}
+                minLength="4"
+                required
+            />
+            {props.state.showSignup && (
+                <div id="color-selector">
+                    <p>pick a color :&nbsp;</p>
                     <input
-                        type="button"
-                        value="LOG IN"
-                        onClick={this.handleClick}
-                        className="button"
-                    />
-                    <input
-                        type="button"
-                        value="close"
-                        onClick={this.props.toggleModal}
-                        className="button"
+                        type="color"
+                        id="color-input"
+                        name="color"
+                        value={props.state.color}
+                        onChange={props.handleChange}
                     />
                 </div>
-                {this.state.showSignup ? (
-                    <div>
-                        <p>Already have an account?</p>
-                        <input
-                            type="button"
-                            value="Log In"
-                            onClick={this.toggleSignup}
-                            className="button"
-                        />
-                    </div>
-                ) : (
-                    <div>
-                        <p>No account yet?</p>
-                        <input
-                            type="button"
-                            value="Sign Up"
-                            onClick={this.toggleSignup}
-                            className="button"
-                        />
-                    </div>
-                )}
+            )}
+            <div id="loginmodal-buttons">
+                <input
+                    type="button"
+                    value={props.state.showSignup ? "Sign Up" : "Log In"}
+                    onClick={props.handleClick}
+                    className="button"
+                />
+                <input
+                    type="button"
+                    value="close"
+                    onClick={props.toggleModal}
+                    className="button"
+                />
             </div>
-        );
-    }
+            <div id="toggle-signup-group">
+                <p>
+                    {props.state.showSignup
+                        ? "Already have an account?"
+                        : "No account yet?"}
+                </p>
+                <input
+                    id="toggle-signup"
+                    type="button"
+                    value={props.state.showSignup ? "Log In" : "Sign Up"}
+                    onClick={props.toggleSignup}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default LoginModal;
--- src/client/components/overlay.js
+++ src/client/components/overlay.js
@@ -7,23 +7,53 @@ import "../styles/overlay.css";
 class Overlay extends React.Component {
     constructor() {
         super();
-        this.state = {showLoginModal: true};
+        this.state = {
+            showLoginModal: true,
+            showSignup: false,
+            email: "",
+            password: "",
+            color: "#FF0000",
+        };
         this.toggleModal = this.toggleModal.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.toggleSignup = this.toggleSignup.bind(this);
     }
     toggleModal() {
         this.setState({showLoginModal: !this.state.showLoginModal});
+    }
+    handleChange(event) {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value,
+        });
+    }
+    toggleSignup() {
+        this.setState({
+            showSignup: !this.state.showSignup,
+        });
+    }
+    handleClick() {
+        console.log("the Class based component method handleClick works");
     }
     render() {
         return (
             <div id="overlay">
                 <TopBar />
-                <Button
-                    value="Log In"
-                    toggleModal={this.toggleModal}
-                    className="button-login"
-                />
+                {!this.state.showLoginModal && (
+                    <Button
+                        value="Log In"
+                        toggleModal={this.toggleModal}
+                        className="button-login"
+                    />
+                )}
                 {this.state.showLoginModal && (
-                    <LoginModal toggleModal={this.toggleModal} />
+                    <LoginModal
+                        state={this.state}
+                        toggleModal={this.toggleModal}
+                        handleChange={this.handleChange}
+                        toggleSignup={this.toggleSignup}
+                        handleClick={this.handleClick}
+                    />
                 )}
             </div>
         );
